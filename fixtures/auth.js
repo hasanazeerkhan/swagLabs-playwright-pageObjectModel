42301aa7bd6c2ccfeaac0819ee912bc54c09dca3
Mohammed Hasan <hasanazeerkhan@gmail.com>
2025-01-21 22:37:35 +0530

    feat: updated user data

---
+++ fixtures/auth.js
@@ -0,0 +1,12 @@
+const credentials = {
+    userName: {
+      user1: "standard_user",
+      user2: "locked_out_user",
+      user3: "problem_user",
+      user4: "performance_glitch_user",
+      user5: "error_user",
+      user6: "visual_user"
+    },
+    password: "secret_sauce",
+  };
+  
